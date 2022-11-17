8eddb5226cb6c6abd8d4a5d2426cf4484bad10ff
Mariam Martirosyan <marus16081988@mail.ru>
2022-11-17 17:46:53 +0400

    commit

--- src/app/layout/MainLayout.tsx
+++ src/app/layout/MainLayout.tsx
@@ -5,17 +5,20 @@ import Sidebar from "../shared/components/Sidebar";
 
 const MainLayout = () => {
   return (
-    <Grid container m={7}>
+    <Grid container m={2}>
       <Grid item lg={3}>
         <Sidebar />
       </Grid>
-      <Grid item style={{ display: "flex", flexDirection: "row" }} >
-        <div>
-          <Outlet />
-        </div>
-        <div style={{width:"100%"}}>
-          <Input />
-        </div>
+      <Grid item lg={9}>
+        <Grid container style={{width:"100%"}}>
+          <Grid item xs={10}>
+          
+            <Outlet />
+          </Grid>
+          <Grid item xs={9}>
+            <Input />
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/app/pages/UserItem/index.tsx
+++ src/app/pages/UserItem/index.tsx
@@ -1,9 +1,14 @@
+import { Grid } from "@mui/material";
 import { useParams } from "react-router-dom";
+import "./style.css";
 
 const UserItem = () => {
   const { id } = useParams();
   return (
     <div className="user">
+      <Grid container>
+        <Grid item></Grid>
+      </Grid>
       UserItem {id}
     </div>
   );
