26e6969b3afcfcce10485dad3771223e7296db66
Mariam Martirosyan <marus16081988@mail.ru>
2022-11-21 15:06:08 +0400

    adding messanges

--- src/app/pages/UserItem/index.tsx
+++ src/app/pages/UserItem/index.tsx
@@ -2,9 +2,11 @@ import { Avatar, Grid, ListItemAvatar, Typography } from "@mui/material";
 import { useParams } from "react-router-dom";
 import Input from "../../shared/components/Input";
 import { usersData } from "../homePage/constants";
+import { userTexts } from "../homePage/constants";
 import SearchIcon from "@mui/icons-material/Search";
 import CallOutlinedIcon from "@mui/icons-material/CallOutlined";
 import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import Triangle from "../../assests/images/triangle.svg";
 import "./style.css";
 
 const UserItem = () => {
@@ -15,7 +17,7 @@ const UserItem = () => {
   return (
     <div className="user">
       <Grid container className="userTop" spacing={0}>
-        <Grid item style={{width:"60px"}}>
+        <Grid item style={{ width: "60px" }}>
           <Avatar src={user?.src} sx={{ width: 45, height: 45 }} />
         </Grid>
         <Grid item xs={8}>
@@ -35,6 +37,30 @@ const UserItem = () => {
             <MoreVertOutlinedIcon />
           </span>
         </div>
+      </Grid>
+      <Grid container className="conversation">
+        {userTexts.map((text) => {
+          return (
+            <>
+              <Grid container my={3} mx={25}>
+                <Grid item className="userAnswer" key={text.id}>
+                  <span className="tail">
+                    <img src={Triangle} alt="triangle" />
+                  </span>
+                  {text?.text} <span className="timeSpan">{text.time}</span>
+                </Grid>
+              </Grid>
+              <Grid container className="myAnswer" key={text.id} mx={90}>
+                <span className="myAnswerSpan">{text?.myAnswer}</span>
+                <span className="myTimeSpan">{text?.myAnswertime}</span>
+
+                <span className="tailMyAnswer">
+                  <img src={Triangle} alt="triangle" />
+                </span>
+              </Grid>
+            </>
+          );
+        })}
       </Grid>
       UserItem {id}
       <Input />
--- src/app/pages/homePage/constants/index.ts
+++ src/app/pages/homePage/constants/index.ts
@@ -12,3 +12,11 @@ export const usersData=[
     {id:9, name:"user3",text:"text3",src:One},
     {id:10, name:"user4",text:"text4",src:One}
 ]
+export const userTexts=[
+    {id:1, text:"skdjn xflkvfndf cl",time:14.13,myAnswer:"kbkjcbsdlvkb kljvj",myAnswertime:14.15},
+    {id:2, text:"barev",time:16.00,myAnswer:"kbkjcpioiiiiiiiiiiiiiiiiiiiibsdlvkb gvhvhbj",myAnswertime:16.12},
+    {id:3, text:"n xbzncb",time:18.11,myAnswer:"kbkjcbsdlvkhgvhk nb kljvj",myAnswertime:18.15},
+    {id:4, text:"nb xmjcn kb n mn bjb kn",time:21.14, myAnswer:"kbkjcbsdlvkb kljvj",myAnswertime:21.15},
+    {id:5, text:"bjcndvjnxvolnsvnjxnn knkml",time:22.17,myAnswer:"kbkjcbsdhvkb kljvj",myAnswertime:22.30},
+    {id:6, text:"kjbkbjn b cdakjnscdzk",time:23.45,myAnswer:"kbkjc",myAnswertime:23.55}
+]
