dadb775b59a38eaf8541baa3e8bfcd7c143d7f85
Mariam Martirosyan <marus16081988@mail.ru>
2022-11-22 10:26:12 +0400

    changing userItem bg

--- src/app/shared/components/Input/index.tsx
+++ src/app/shared/components/Input/index.tsx
@@ -1,29 +1,55 @@
 import { TextField } from "@mui/material";
-//import { Controller } from "react-hook-form";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import MicNoneIcon from "@mui/icons-material/MicNone";
-import AttachFileIcon from '@mui/icons-material/AttachFile';
-import SentimentSatisfiedAltIcon from '@mui/icons-material/SentimentSatisfiedAlt';
+import AttachFileIcon from "@mui/icons-material/AttachFile";
+import SentimentSatisfiedAltIcon from "@mui/icons-material/SentimentSatisfiedAlt";
 import "./style.css";
 
+interface IFormData {
+  text: string;
+}
 const Input = () => {
+  const methods = useForm<IFormData>({
+    defaultValues: {
+      text: "",
+    },
+  });
+
+  const { handleSubmit, control } = methods;
+
+  const onSubmit = (formData: IFormData) => {
+    const newFormData = {
+      text: formData.text,
+    };
+    console.log(newFormData);
+  };
+
   return (
     <div className="inputMain">
       <div className="input">
-        {/* <Controller
-        control={control}
-        render={({ field }) => ( */}
-        <TextField
-          //{...field}
-          //value={field.value ?? ""g}
-          fullWidth
-          multiline={true}
-          variant="outlined"
-        />
-         <div className="smileIcon"> <SentimentSatisfiedAltIcon/></div>
-        <div className="attachIcon"> <AttachFileIcon/></div>
+          <Controller
+            control={control}
+            name="text"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                fullWidth
+                className="inputField"
+                multiline={true}
+                variant="outlined"
+              />
+              
+            )}
+          />
+        <div className="smileIcon">
+          <SentimentSatisfiedAltIcon />
+        </div>
+        <div className="attachIcon">
+          <AttachFileIcon />
+        </div>
       </div>
       <div className="inputIcon">
-        <MicNoneIcon />
+        <MicNoneIcon onClick={handleSubmit(onSubmit)} />
       </div>
     </div>
   );
